be241b7386a34e486012bf7ce163dbc457ee989f
Shevja <47684462+Shevja@users.noreply.github.com>
2020-05-07 21:48:47 +0700

    Update esc.cpp

--- esc.cpp
+++ esc.cpp
@@ -8,15 +8,24 @@ using namespace std;
 int main(){
 	mt_clrscr();
 
-	int x = 20, y = 20, cols = 0, rows = 0;
+	int x = 5, y = 10, cols = 0, rows = 0;
 	mt_getScreen(&rows, &cols);
 	mt_gotoXY(x, y, rows, cols);
 
-	color b(NAVY);
-	mt_setbgcolor(b);
-	color f(GREEN);
-	mt_setfgcolor(f);
-	cout << "gg" << endl;
+	mt_setbgcolor(BLACK);
+	mt_setfgcolor(RED);
+	cout << "Kashirin Vladislav" << endl;
+
+	x = 6, y = 8;
+	mt_gotoXY(x, y, rows, cols);
+
+	mt_setbgcolor(GREEN);
+	mt_setfgcolor(WHITE);
+	cout << "IV822" << endl;
+
+	mt_gotoXY(10, 1, rows, cols);
+	mt_setbgcolor(DEFAULT);
+	mt_setfgcolor(DEFAULT);
 
 	cursor_invis();
 	cursor_vis();
